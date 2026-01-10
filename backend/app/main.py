6a81085c99b3760577ffb31f180dec16fc0c978d
Gokul-pixel-1 <gokulsenthilganesh1@gmail.com>
2026-01-10 08:30:49 +0530

    Update main.py

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+from backend.app.routes.leads import router as leads_router
+from backend.app.routes.calls import router as calls_router
+from backend.app.routes.analytics import router as analytics_router
+from backend.app.routes.users import router as users_router
+from backend.app.routes.upload import router as upload_router
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "backend working"}
+
+app.include_router(upload_router)
+app.include_router(leads_router)
+app.include_router(calls_router)
+app.include_router(analytics_router)
+app.include_router(users_router)
